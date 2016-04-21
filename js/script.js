5e6c667a61c850e04151ad3ee1d24c7ea6cd7818
malsf21 <matthewang.wang@gmail.com>
2016-04-21 12:48:47 -0400

    adds the very basics

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function checkBirthday(name){
+  var currentDate = new Date();
+  var currentDay = currentDate.getDate();
+  var currentMonth = currentDate.getMonth();
+  $('#header').html("<h3>Is it " + name + "'s Birthday?</h3>");
+  if (currentDay == 21 && currentMonth == 3){
+    $('#message').html("<h1 class='display-1'>It is! Happy Birthday " + name + "!</h1>");
+    var music = document.createElement('audio');
+    music.setAttribute('src', 'resources/september.mp3');
+    music.loop = true;
+    music.currentTime = 10;
+    music.play();
+  }
+  else{
+    $('#message').html("Nope, it isn't " + name + "'s Birthday today.");
+  }
+}
