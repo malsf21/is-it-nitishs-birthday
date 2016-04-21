7d34194ac537cc0c8eac93080b2a70e6d9ddea0d
malsf21 <matthewang.wang@gmail.com>
2016-04-21 14:34:56 -0400

    modularizes stuff

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-function checkBirthday(name){
+function checkBirthday(name, month, day){ //name is a string, month is a number, day is a number
   var currentDate = new Date();
   var currentDay = currentDate.getDate();
   var currentMonth = currentDate.getMonth();
   $('#header').html("<h3>Is it " + name + "'s Birthday?</h3>");
-  if (currentDay == 21 && currentMonth == 3){
+  if (currentDay == day && currentMonth == month-1){
     $('#message').html("<h1 class='display-1'>It is! Happy Birthday " + name + "!</h1>");
     var music = document.createElement('audio');
     music.setAttribute('src', 'resources/september.mp3');
@@ -13,7 +13,7 @@ function checkBirthday(name){
     $('#container').addClass("bg-yes");
   }
   else{
-    $('#message').html("Nope, it isn't " + name + "'s Birthday today.");
+    $('#message').html("Nope, it isn't " + name + "'s Birthday today. </br><h1 class='display-3'>Wait until " + month + "/" + day + " to celebrate it!");
     $('#container').addClass("bg-no");
   }
 }
